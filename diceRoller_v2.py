3984758411fdf6f87ad48737d3fd0eb09f53e90e
kjhill20404 <51007611+kjhill20404@users.noreply.github.com>
2023-03-19 16:41:00 -0500

    Add files via upload

--- diceRoller_v2.py
+++ diceRoller_v2.py
@@ -2,7 +2,7 @@ import random
 
 import time
 
-from gui_test import infoEntry, printInfo
+#from gui_test import infoEntry, #printInfo
 from json_py_test import *
 
 prevRoll = []
@@ -15,16 +15,18 @@ options = ["roll", "last roll", "info", "create"]
 class roll:
     def dice(times,sides):
         timesSoFar = 0
+        currentRoll = []
         while timesSoFar != int(times):
-            currentRoll = random.randint(1, int(sides))
+
+            currentRoll.append(random.randint(1, int(sides)))
             prevRoll.append(currentRoll)
-            printInfo(currentRoll)
+            #printInfo(currentRoll)
             timesSoFar = timesSoFar + 1
         return currentRoll
     def lastRoll():
-        printInfo(prevRoll)
-        for e in prevRoll:
-            printInfo(e)
+        #printInfo(prevRoll)
+        # for e in prevRoll:
+        #     printInfo(e)
         return prevRoll
 
 def requestMaker():
@@ -33,7 +35,7 @@ def requestMaker():
 
     action = requestSeporated[0]
     if action != options[0] and type(requestSeporated[0].partition("d") )== str() :
-        printInfo(action)
+        #printInfo(action)
         dReq = False
     if action == "last" and requestSeporated[1] == "roll":
         roll.lastRoll()
@@ -42,19 +44,19 @@ def requestMaker():
     elif action == options[3]:
         createChar()
     elif action == options[0] or dReq == True:
-        printInfo(type(requestSeporated[0].partition("d") ))
+        #printInfo(type(requestSeporated[0].partition("d") ))
         dReq = True
         if action == "roll":
             a = requestSeporated[1].partition("d") #command used to seporate a string into another list
         else:
             a = requestSeporated[0].partition("d")
 
-        printInfo(a)
+        #printInfo(a)
         prevRoll = []
         roll.dice(a[0], a[2])
 
 def mainLoop(rq):
-    printInfo(rq)
+    #printInfo(rq)
     run = True
     while(run == True):
         global request
@@ -64,4 +66,4 @@ def mainLoop(rq):
         else:
             requestMaker()
 
-mainLoop(infoEntry.getInfo())
+# mainLoop(infoEntry.getInfo())
--- gui_test.py
+++ gui_test.py
@@ -1,9 +1,11 @@
 import tkinter as tk
-
+from diceRoller_v2 import roll
 questens = ["what could i do for you?"]
 
+var= ["printFrame","rollButton","backButton","tb","addButton1","addButton2","subtractButton1","subtractButton2","timesNumLabel","timesFrame","timesLabel1","timesLabel2"]
+
 class infoEntry:
-    def getInfo():
+    def getInfo(entry):
         global request
         request = entry.get()
         entry.delete(0, tk.END)
@@ -28,43 +30,166 @@ class ratio:
 index = 0
 
 def labelText(questen):
+    global questenLabel
     questenLabel = tk.Label(text = questen)
     questenLabel.grid(row = 0, column = 0, sticky = "e",)
 
+def TimesIncrease():
+    value = int(timesNumLabel["text"])
+    timesNumLabel["text"] = f"{value + 1}"
+
+def TimesDecrease():
+    value = int(timesNumLabel["text"])
+    timesNumLabel["text"] = f"{value - 1}"
+
+def sidesIncrease():
+    value = int(sidesNumLabel["text"])
+    sidesNumLabel["text"] = f"{value + 1}"
+
+def sidesDecrease():
+    value = int(sidesNumLabel["text"])
+    sidesNumLabel["text"] = f"{value - 1}"
 
 
-def printInfo(event):
-    global request
-    request = entry.get()
-    entry.delete(0, tk.END)
+def addRoll():
+    global sides
+    sides = int(sidesNumLabel["text"])
+    global times
+    times = int(timesNumLabel["text"])
+    global roll
+    endRoll = roll.dice(times,sides)
+    sidesNumLabel["text"] = 0
+    timesNumLabel["text"] = 0
     tb["state"] = "normal"
-    # tb.delete("1.0", tk.END)
-    tb.insert(tk.END,request+"\n" )
+    tb.insert(tk.END,str(endRoll)+"\n" )
     tb["state"] = "disabled"
-    # label2.grid(row = 1, column = 0, sticky = "e",)
     tb.pack()
 
+def diceSection():
+    diceSectionButton.grid_forget()
+    labelText("how many sides would you like?")
+
+    global sidesFrame
+    sidesFrame = tk.Frame(window)
+    sidesFrame.grid(row = 0, column = 1)
+
+    global sidesNumLabel
+    global sidesNum
+    sidesNum = 0
+    sidesNumLabel = tk.Label(master = sidesFrame, text = str(sidesNum))
+    sidesNumLabel.grid(row = 0, column = 1)
+
+    global timesLabel1
+    timesLabel1 = tk.Label(text = "how many times would you like to roll the dice?")
+    timesLabel1.grid(row = 1, column = 0, sticky = "e",)
+
+    global subtractButton1
+    subtractButton1 = tk.Button(master = sidesFrame, text = "-", command = sidesDecrease)
+    subtractButton1.grid(row = 0, column = 0)
+
+    global addButton1
+    addButton1 = tk.Button(master = sidesFrame, text = "+", command = sidesIncrease)
+    addButton1.grid(row = 0, column = 2)
+
+    global timesFrame
+    timesFrame = tk.Frame(window)
+    timesFrame.grid(row = 1, column = 1)
+
+    global timesNumLabel
+    global timesNum
+    timesNum = 0
+    timesNumLabel = tk.Label(master = timesFrame, text = str(timesNum))
+    timesNumLabel.grid(row = 0, column = 1)
+
+    global timesLabel2
+    timesLabel2 = tk.Label(text = "how many times would you like to roll the dice?")
+    timesLabel2.grid(row = 1, column = 0, sticky = "e",)
+
+    global subtractButton2
+    subtractButton2 = tk.Button(master = timesFrame, text = "-", command = TimesDecrease)
+    subtractButton2.grid(row = 0, column = 0)
+
+    global addButton2
+    addButton2 = tk.Button(master = timesFrame, text = "+", command = TimesIncrease)
+    addButton2.grid(row = 0, column = 2)
+
+    global printFrame
+    printFrame = tk.Frame(window,width=200, height=115)
+    printFrame.grid(row = 2,column = 1, sticky = "e")
+
+    global tb
+    tb = tk.Text(master = printFrame)
+    tb["state"] = "disabled"
+    tb.pack()
+
+    global rollButton
+    rollButton = tk.Button(master = window, text = "Roll" , command = addRoll)
+    rollButton.grid(row = 3, column = 2, sticky = "e")
+
+    global backButton
+    backButton = tk.Button(master = window, text = "back" ,command = windowDestroy)
+    backButton.grid(row = 3, column = 0, sticky = "e")
+
+def windowDestroy():
+    i = 0
+    sidesFrame.grid_forget()
+    questenLabel.grid_forget()
+    sidesNumLabel.grid_forget()
+    printFrame.grid_forget()
+    rollButton.grid_forget()
+    backButton.grid_forget()
+    tb.grid_forget()
+    addButton1.grid_forget()
+    addButton2.grid_forget()
+    subtractButton1.grid_forget()
+    subtractButton2.grid_forget()
+    timesNumLabel.grid_forget()
+    timesFrame.grid_forget()
+    timesLabel1.grid_forget()
+    timesLabel2.grid_forget()
+    diceSectionButton.grid(row = 0, column = 0, sticky = "e")
+
+def TimesIncrease():
+    value = int(timesNumLabel["text"])
+    timesNumLabel["text"] = f"{value + 1}"
+
+def TimesDecrease():
+    value = int(timesNumLabel["text"])
+    timesNumLabel["text"] = f"{value - 1}"
+
+def sidesIncrease():
+    value = int(sidesNumLabel["text"])
+    sidesNumLabel["text"] = f"{value + 1}"
+
+def sidesDecrease():
+    value = int(sidesNumLabel["text"])
+    sidesNumLabel["text"] = f"{value - 1}"
 
 
+def addRoll():
+    global sides
+    sides = int(sidesNumLabel["text"])
+    global times
+    times = int(timesNumLabel["text"])
+    global roll
+    endRoll = roll.dice(times,sides)
+    sidesNumLabel["text"] = 0
+    timesNumLabel["text"] = 0
+    tb["state"] = "normal"
+    tb.insert(tk.END,str(endRoll)+"\n" )
+    tb["state"] = "disabled"
+    tb.pack()
 window = tk.Tk()
 window.title("quick test")
-# window.geometry("700x500")
-
-labelText("what could I do for you?")
-
-entry = tk.Entry(width=20)
-entry.grid(row = 0, column = 1)
 
 
-printFrame = tk.Frame(window,width=375, height=115)
-printFrame.grid(row = 1,column = 1, sticky = "e")
-tb = tk.Text(master = printFrame)
-tb["state"] = "disabled"
-tb.pack()
+activeFrame = tk.Frame(window)
+activeFrame.grid(row = 0, column = 0)
+
+diceSectionButton = tk.Button(master = activeFrame, text = "Roll" , command = diceSection)
+diceSectionButton.grid(row = 0, column = 0, sticky = "e")
 
 
-window.bind("<Return>", printInfo)
-print(infoEntry.getInfo())
 
 
 window.mainloop()
